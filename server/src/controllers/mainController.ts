4e91bb8fc16717f68d640a0cd10e7ac38ea279aa
iafan1229 <home0@naver.com>
2025-03-27 18:07:51 +0900

    #10 fix. node.js - 불필요한 컴포넌트 정리

--- server/src/controllers/mainController.ts
+++ server/src/controllers/mainController.ts
@@ -26,42 +26,4 @@ const list = async (_: never, res: Response<ResponseData>) => {
   }
 };
 
-const email = async (req: any, res: any) => {
-  const { to, subject, message } = req.body;
-  const htmlContent = `
-  <div style="font-family: Arial, sans-serif; line-height: 1.5;">
-    <h1 style="color: #4CAF50;">Hello!</h1>
-    <p>${message}</p>
-    <footer style="margin-top: 20px; color: #888;">
-      <p>Best regards,</p>
-      <p><strong>Your Company</strong></p>
-    </footer>
-  </div>
-`;
-
-  // Nodemailer Transporter
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // upgrade later with STARTTLS
-    auth: {
-      user: "iafan103603@gmail.com",
-      pass: "rzrb tmdr idxj sesp",
-    },
-  });
-
-  try {
-    await transporter.sendMail({
-      from: "your-email@gmail.com",
-      to,
-      subject,
-      html: htmlContent,
-    });
-    res.status(200).send("Email sent successfully!");
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).send("Failed to send email.");
-  }
-};
-
-module.exports = { slider, list, email };
+module.exports = { slider, list };
