b24365c85cb7711227f8b94419dc4c6b609db88e
iafan1229 <home0@naver.com>
2025-04-11 13:39:44 +0900

    #14 fix. flask 디버깅 - 셀레니움 에러 - chrome driver 수정

--- flask/driver.py
+++ flask/driver.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-
 
 # 전역 드라이버 변수
 driver = None
-
 
 def start_driver():
     global driver
     if driver is None:
         options = Options()
-        options.add_argument("--headless")  # 헤드리스 모드
-        service = Service(executable_path=ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service,options=options)
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        
+        # Docker 환경에서는 ChromeDriverManager 대신 기본 경로 사용
+        driver = webdriver.Chrome(options=options)
         driver.set_window_size(800, 600)
     return driver
 
